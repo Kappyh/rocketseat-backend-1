2dddcc9e3445be0bd9eced7208be03619f993cf9
Gabriela <gabriela@MacBook-Pro-de-Gabriela.local>
2021-01-27 22:28:21 -0300

    app exemplo funcional

--- src/index.js
+++ src/index.js
@@ -1,29 +1,50 @@
+const { request } = require("express");
 const express = require("express");
-
+const { uuid } = require("uuidv4");
 const app = express();
 // precisa informar
 app.use(express.json());
 
+const projects = [];
+
 app.get("/projects", (request, response) => {
-  const { title, owner } = request.query;
-  console.log(title, " ", owner);
-  return response.json(["projeto1", "projeto2"]);
+  const { title } = request.query;
+  const results = title
+    ? projects.filter(project => project.title.includes(title))
+    : projects;
+  return response.json(results);
 });
 
 app.post("/projects", (request, response) => {
-  const body = request.body;
-  console.log(body);
-  return response.json(["projeto1", "projeto2", "projeto3"]);
+  const { title, owner } = request.body;
+  const project = { id: uuid(), title, owner };
+  projects.push(project);
+  return response.json(project);
 });
 
 app.put("/projects/:id", (request, response) => {
   const { id } = request.params;
-  console.log(id);
-  return response.json(["projeto4", "projeto2", "projeto3"]);
+  const { title, owner } = request.body;
+
+  const projectIndex = projects.findIndex((project) => project.id === id);
+  if (projectIndex < 0) {
+    return response.status(404).json({ error: "project not found" });
+  }
+  const project = { id, title, owner };
+  projects[projectIndex] = project;
+  return response.json(project);
 });
 
 app.delete("/projects/:id", (request, response) => {
-  return response.json(["projeto1", "projeto3"]);
+  const { id } = request.params;
+  const projectIndex = projects.findIndex((project) => project.id === id);
+  if (projectIndex < 0) {
+    return response.status(404).json({ error: "project not found" });
+  }
+
+  projects.splice(projectIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.listen(3333, () => {
