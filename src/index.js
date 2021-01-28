c5e4d2b27914997f127bf4525e791afb398d2631
Gabriela <gabriela@MacBook-Pro-de-Gabriela.local>
2021-01-27 22:03:41 -0300

    metodos http

--- src/index.js
+++ src/index.js
@@ -3,8 +3,21 @@ const express = require('express');
 const app = express();
 
 app.get('/projects',(request,response)=>{
-  return response.json({message: 'Hello Kira'});
-})
+  return response.json(['projeto1','projeto2']);
+});
+
+app.post('/projects', (request, response)=>{
+  return response.json(['projeto1','projeto2','projeto3']);
+});
+
+app.put('/projects/:id', (request, response)=>{
+  return response.json(['projeto4','projeto2','projeto3']);
+});
+
+app.delete('/projects/:id', (request, response)=>{
+  return response.json(['projeto1','projeto3']);
+});
+
 app.listen(3333,()=>{
   console.info('⚙️ Backend started!');
 });
