dd0fee967a31276f1c6dbf30fa86bcec939214e1
Gabriela <gabriela@MacBook-Pro-de-Gabriela.local>
2021-01-28 21:53:44 -0300

    middlewares mais exemplos

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 const app = express();
 // precisa informar
 app.use(express.json());
 
 const projects = [];
 
-function logRequests(request,response,next){
+function logRequests(request, response, next) {
   const { method, url } = request;
 
   const logLabel = `[${method.toUpperCase()} ${url}]`;
@@ -16,12 +16,21 @@ function logRequests(request,response,next){
   return next();
 }
 
+function validateProjectId(request, response, next) {
+  const { id } = request.params;
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: "Invalid project ID." });
+  }
+  return next();
+}
+
 app.use(logRequests);
+app.use("/projects:id", validateProjectId);
 
 app.get("/projects", (request, response) => {
   const { title } = request.query;
   const results = title
-    ? projects.filter(project => project.title.includes(title))
+    ? projects.filter((project) => project.title.includes(title))
     : projects;
   return response.json(results);
 });
